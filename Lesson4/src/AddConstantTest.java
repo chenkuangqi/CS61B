8019d23f9a53e31f6c071572b589cda8beddd3ab
chenkuangqi <chenkuangqi#@outlook.com>
2022-10-17 10:18:34 -0700

    Oct16

---
+++ Lesson4/src/AddConstantTest.java
@@ -0,0 +1,29 @@
+package IntList;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+public class AddConstantTest {
+
+    @Test
+    public void testAddConstantOne() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5);
+        IntListExercises.addConstant(lst, 1);
+        assertEquals("2 -> 3 -> 4 -> 5 -> 6", lst.toString());
+    }
+
+    @Test
+    public void testAddConstantTwo() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5);
+        IntListExercises.addConstant(lst, 2);
+        assertEquals("3 -> 4 -> 5 -> 6 -> 7", lst.toString());
+    }
+
+    @Test
+    public void testAddToLargeList() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        IntListExercises.addConstant(lst, 10);
+        assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
+    }
+}
---
+++ Lesson4/src/IntList.java
@@ -0,0 +1,41 @@
+package IntList;
+
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r){
+        first = f;
+        rest = r;
+
+    }
+
+    public int size() {
+         if ( rest == null){
+             return 1;
+         }
+         return 1+this.rest.size();
+    }
+
+    public int iterativesize(){
+        IntList p = this;
+        int totalsize = 0;
+        while (p != null){
+            totalsize +=1;
+            p = p.rest;
+        }
+        return totalsize;
+    }
+
+
+    public int get(int i){
+        if (i == 0 ){
+            return first;
+        }
+        return this.rest.get(i-1);
+    }
+    public static void main(String[] args){
+        IntList L = new IntList(10,null);
+
+    }
+}
